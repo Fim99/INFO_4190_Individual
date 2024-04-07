2a787b121d7e21a5ad769e56aa9af4e002b66d2f
Fim99 <nathandverma@gmail.com>
2024-04-06 19:48:29 -0700

    Added basic multiplication method

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,20 @@
 public class Calculator 
-{
-    public static void main(String[] args) 
+{   
+    // Method to perform multiplication
+    public double multiply(double num_1, double num_2) 
     {
+        return num_1 * num_2;
+    }
+
+    public static void main(String[] args) 
+    {   
+        // New Calculator Object
         Calculator calculator = new Calculator();
+        
+        double mult_1 = 5;
+        double mult_2 = 3;
+
+        double result_1 = calculator.multiply(mult_1, mult_2);
+        System.out.println(mult_1 + " x " + mult_2 + " = " + result_1);
     }
 }
